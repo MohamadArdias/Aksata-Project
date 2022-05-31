9d0ff49d027b56b4a6e73db0a3e5ba8142b67208
MohamadArdias <71806525+MohamadArdias@users.noreply.github.com>
2022-05-31 21:41:51 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -72,6 +72,7 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
+  <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
   <script src="{{ asset('style/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
   <script src="{{ asset('style/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{ asset('style/assets/vendor/chart.js/chart.min.js')}}"></script>
